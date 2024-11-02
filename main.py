e639879015fbe32122cb03f17fc0120ed8dbe4af
x500x <1033652712@qq.com>
2024-11-02 22:56:28 +0800

    updata some files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from DrissionPage import Chromium
 
 tab = Chromium().latest_tab
-tab.set.cookies("PHPSESSID=7lmrlred0bc04kpl21j4r9i8f3; PTCMS_history=21360%2C98080; PTCMS_comeurl=%2F; PTCMS_userid=39023; PTCMS_username=1033652712%40qq.com; PTCMS_usernames=%E5%90%AC%E5%8F%8B_28222; PTCMS_token=06061e4a35432ebc033907adb3cfb380; PTCMS_logintime=1730558324")
+tab.set.cookies("PHPSESSID=7lmrlred0bc04kpl21j4r9i8f3; PTCMS_history=21360,98080; PTCMS_comeurl=/; PTCMS_userid=39023; PTCMS_username=1033652712@qq.com; PTCMS_usernames=听友_28222; PTCMS_token=06061e4a35432ebc033907adb3cfb380; PTCMS_logintime=1730558324")
 
 # tab.listen.start('m.ting13.cc/api/mapi/play')  # 开始监听，指定获取包含该文本的数据包
 tab.listen.start('mp3m.ysxs.top/' , method=('POST'))  # 开始监听，指定获取包含该文本的数据包
