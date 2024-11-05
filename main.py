8d3180b0955fd85f090d4e08de832e305c0b2262
x500x <1033652712@qq.com>
2024-11-05 23:01:26 +0800

    updata some files

--- main.py
+++ main.py
@@ -6,42 +6,7 @@ import time
 import sys
 # import re
 
-
-tab = Chromium().latest_tab
-cookies="domain=m.ting13.cc;PHPSESSID=91drha7467okr72hb7jh946bf5; PTCMS_history=19170%2C95267%7C21360%2C98080; PTCMS_comeurl=%2F; PTCMS_userid=39023; PTCMS_username=1033652712%40qq.com; PTCMS_usernames=%E5%90%AC%E5%8F%8B_28222; PTCMS_token=6690e87104404eb5c923c6a3e50947a6; PTCMS_logintime=1730780762"
-
-tab.set.cookies(cookies)
-# tab.listen.start('m.ting13.cc/api/mapi/play')  # 开始监听，指定获取包含该文本的数据包
-
-#tab.get('https://m.ting13.cc/play/21360_1_98080.html')  # 访问网址，这行产生的数据包不监听
-
-cid=98081
-
-i=0
-# Time=time.time()
-# flag=-1
-
-tab.listen.start('m.ting13.cc/api/mapi/play')  # 开始监听，指定获取包含该文本的数据包
-tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
-cid+=1
-# res = tab.listen.wait()  # 等待并获取一个数据包
-# print(res.url)  # 打印数据包url
-# print(res)
-
-# tab.listen.wait_silent(timeout=60*3)
-#for res in tab.listen.steps(timeout=60*3):
-for packet in tab.listen.steps(timeout=30):
-    if cid>98091: break
-    tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
-    cid+=1
-    # while time.time()-Time>=5:
-        # Time=time.time()
-        # tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
-        # cid+=1
-        # flag=-1
-    # else:
-        # flag=1
-# res = tab.listen.wait(timeout=60)  # 等待并获取一个数据包
+def GetUrl(packet):
     print(packet.url)  # 打印数据包url
     print(packet.response.status)
     # print(res.response.headers)
@@ -52,7 +17,6 @@ for packet in tab.listen.steps(timeout=30):
     print(packet.request.method)
     # print(res.request.headers)
     print(packet.request.postData)
-    
     url="https://m.ting13.cc/api/mapi/play"
     
     rheardes=packet.request.headers
@@ -77,22 +41,69 @@ for packet in tab.listen.steps(timeout=30):
     response = requests.post(url, params=packet.request.postData, headers=headers)
     
     print(response.text)
+    return response
+
+tab = Chromium().latest_tab
+cookies="domain=m.ting13.cc;PHPSESSID=91drha7467okr72hb7jh946bf5; PTCMS_history=19170%2C95267%7C21360%2C98080; PTCMS_comeurl=%2F; PTCMS_userid=39023; PTCMS_username=1033652712%40qq.com; PTCMS_usernames=%E5%90%AC%E5%8F%8B_28222; PTCMS_token=6690e87104404eb5c923c6a3e50947a6; PTCMS_logintime=1730780762"
+
+tab.set.cookies(cookies)
+# tab.listen.start('m.ting13.cc/api/mapi/play')  # 开始监听，指定获取包含该文本的数据包
+
+#tab.get('https://m.ting13.cc/play/21360_1_98080.html')  # 访问网址，这行产生的数据包不监听
+
+cid=98083
+
+i=0
+
+
+# Time=time.time()
+# flag=-1
+
+tab.listen.start('m.ting13.cc/api/mapi/play')  # 开始监听，指定获取包含该文本的数据包
+tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
+cid+=1
+# res = tab.listen.wait()  # 等待并获取一个数据包
+# print(res.url)  # 打印数据包url
+# print(res)
+
+# tab.listen.wait_silent(timeout=60*3)
+#for res in tab.listen.steps(timeout=60*3):
+for packet in tab.listen.steps(timeout=30):
+    if cid>98091: break
+    tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
+    cid+=1
+    # while time.time()-Time>=5:
+        # Time=time.time()
+        # tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
+        # cid+=1
+        # flag=-1
+    # else:
+        # flag=1
+# res = tab.listen.wait(timeout=60)  # 等待并获取一个数据包
+    
+    
+    response=GetUrl(packet)
     
     res=response.text.encode('utf-8').decode("unicode_escape").replace("\\","")
     jresponse=json.loads(res)
     while jresponse['status']==300:
-        time.sleep(60)
-        response = requests.post(url, params=packet.request.postData, headers=headers)
+        time.sleep(10)
+        tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
+        packet=tab.listen.wait()
+        # rheardes=packet.request.headers
+        # # headers['sp']=
+        # response = requests.post(url, params=packet.request.postData, headers=headers)
+        response=GetUrl(packet)
         print("********************************")
         print(time.time())
-        print(response.text)
         res=response.text.encode('utf-8').decode("unicode_escape").replace("\\","")
         jresponse=json.loads(res)
         i+=1
         if i>2:
             print("i>2,exiting");
             sys.exit()
-    # with open('C:\\info.txt', 'a',encoding="utf-8") as f:
+        
+     # with open('C:\\info.txt', 'a',encoding="utf-8") as f:
     i=0
     with open('C:\\info.txt', 'a',encoding="utf-8") as f:
         # f.write(jresponse['name'].encode('utf-8').decode("unicode_escape").replace("\\",""))
