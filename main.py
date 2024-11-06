c62721c26ab1f37a7af7705a438974cba9d6d99a
x500x <1033652712@qq.com>
2024-11-06 12:28:00 +0800

    updata some files

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import json
 import time
 import sys
-import threading
+# import threading
 # import re
 
 
@@ -46,24 +46,24 @@ def GetUrl(packet):
     print(response.text)
     return response
 
-flag=-1
-cid=98085
+# flag=-1
+cid=98086
 i=0
 
-def NextWebPage():
-    global cid
-    tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
-    cid+=1
-    if cid<=98091: 
-        t1 = threading.Timer(5,NextWebPage)
-         # 启动线程
-        t1.start()
-    else:
-        global flag
-        flag=1
+# def NextWebPage():
+    # global cid
+    # tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
+    # cid+=1
+    # if cid<=98091: 
+        # t1 = threading.Timer(30,NextWebPage)
+         # # 启动线程
+        # t1.start()
+    # else:
+        # global flag
+        # flag=1
     
 tab = Chromium().latest_tab
-cookies="domain=m.ting13.cc;PHPSESSID=91drha7467okr72hb7jh946bf5; PTCMS_history=19170%2C95267%7C21360%2C98080; PTCMS_comeurl=%2F; PTCMS_userid=39023; PTCMS_username=1033652712%40qq.com; PTCMS_usernames=%E5%90%AC%E5%8F%8B_28222; PTCMS_token=6690e87104404eb5c923c6a3e50947a6; PTCMS_logintime=1730780762"
+cookies="domain=m.ting13.cc;PHPSESSID=91drha7467okr72hb7jh946bf5; PTCMS_history=19170%2C95267%7C21360%2C98080; PTCMS_comeurl=%2F; PTCMS_userid=39023; PTCMS_username=1033652712%40qq.com; PTCMS_usernames=%E5%90%AC%E5%8F%8B_28222; PTCMS_token=befcece4cd440e935c1ab73976d70018; PTCMS_logintime=1730866980"
 
 tab.set.cookies(cookies)
 # tab.listen.start('m.ting13.cc/api/mapi/play')  # 开始监听，指定获取包含该文本的数据包
@@ -73,20 +73,20 @@ tab.set.cookies(cookies)
 
 
 
-# Time=time.time()
+Time=time.time()
 # flag=-1
 
 tab.listen.start('m.ting13.cc/api/mapi/play')  # 开始监听，指定获取包含该文本的数据包
-NextWebPage()
-# tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
-# cid+=1
+# NextWebPage()
+tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
+cid+=1
 # res = tab.listen.wait()  # 等待并获取一个数据包
 # print(res.url)  # 打印数据包url
 # print(res)
 
 # tab.listen.wait_silent(timeout=60*3)
 #for res in tab.listen.steps(timeout=60*3):
-for packet in tab.listen.steps(timeout=30):
+for packet in tab.listen.steps(timeout=60):
     
     
     # while time.time()-Time>=5:
@@ -103,9 +103,11 @@ for packet in tab.listen.steps(timeout=30):
     
     res=response.text.encode('utf-8').decode("unicode_escape").replace("\\","")
     jresponse=json.loads(res)
-    while jresponse['status']==300:
-        time.sleep(10)
+    while jresponse['status']==300:#to do
+        time.sleep(30)
         tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
+        cid+=1
+        Time=time.time()
         packet=tab.listen.wait()
         # rheardes=packet.request.headers
         # # headers['sp']=
@@ -131,7 +133,16 @@ for packet in tab.listen.steps(timeout=30):
         f.write('\n')
         f.write(jresponse['url'])
         f.write('\n')
-    if tab.listen.wait(timeout=0)==False and flag>0: break
+    
+    if time.time()-Time<30:
+        time.sleep(30-(time.time()-Time))
+    tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
+    cid+=1
+    Time=time.time()
+    
+    
+    
+    # if tab.listen.wait(timeout=0)==False and flag>0: break
     # tab.get(f"https://m.ting13.cc/play/21360_1_{cid}.html")
     # cid+=1
             # f.write('\n')
